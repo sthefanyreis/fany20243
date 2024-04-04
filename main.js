e332b0cd4de92a9a59296f3691797ff37d284f48
sthefany <110991751+sthefanyreis@users.noreply.github.com>
2024-04-04 08:08:03 -0300

    04/03

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 const botoes=document.querySelectorAll(".botao");
 console.log(botoes)
 for (let i=0; i < botoes.length;i++) {
-    console.log(i);
-}
-for(let i=0;i <botoes.length;i++){
     botoes[i].onclick=function(){
         for(let j=0;j<botoes.length;j++){
             botoes[j].classList.remove("ativa");

e6f149a8e7b9370c594cd9c113ef1f4f9f232976
sthefany <110991751+sthefanyreis@users.noreply.github.com>
2024-04-11 08:08:27 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ console.log(botoes)
 for (let i=0; i < botoes.length;i++) {
     botoes[i].onclick=function(){
         for(let j=0;j<botoes.length;j++){
-            botoes[j].classList.remove("ativa");
+            botoes[j].classList.remove("ativo");
         }
         botoes[i].classList.add("ativo");
     }

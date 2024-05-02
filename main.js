9a5f00c21c6447bd4ced8dd4d4830a14fc3dbda9
sthefany <110991751+sthefanyreis@users.noreply.github.com>
2024-05-02 09:45:26 -0300

    02

--- main.js
+++ main.js
@@ -1,10 +1,32 @@
-const botoes=document.querySelectorAll(".botao");
-console.log(botoes)
-for (let i=0; i < botoes.length;i++) {
-    botoes[i].onclick=function(){
-        for(let j=0;j<botoes.length;j++){
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+
+        for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
         }
+
         botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
     }
 }
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+let tempoAtual = new Date();
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+function calculaTempo(tempoObjetivo) {
+  let tempoAtual = new Date();
+  let tempoFinal = tempoObjetivo - tempoAtual;
+  let segundos = tempoFinal / 1000;
+  return segundos;
+}
+
+
